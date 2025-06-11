7fd90d47ce9e3a832ba96c55de80a11e1f781b33
willian_gmg <willian.gattass@sou.ufmt.br>
2025-06-10 23:44:38 -0400

    Atualiza a estrutura do HTML, adiciona ícone personalizado e altera o título da página. Adiciona dependências do Bootstrap e React-Bootstrap no package.json. Ajusta importações e estrutura do componente Home, incluindo a imagem do Mario.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,27 @@
-import React from "react";
-// import marioImage from "./fotos/marioCruzado.png"; // Adjust the path as necessary
+import marioImage from "../components/fotos/marioCruzado.png"; // Adjust the path as necessary
 import ButtonHome from "../components/ButtonHome"; // Adjust the path as necessary
 import styles from "./home.module.scss"; // Adjust the path as necessary
 
 function Home() {
   return (
     <>
-      <div className={styles.homeContainer}>
-        <h1>Bem vindo ao empresário Mario Spalatti</h1>
-        <h1>Welcome to businessman Mario Spalatti</h1>
-        <ButtonHome text="Sobre mim" onClick={() => alert("Sobre mim")} className="sobreMim" />
-        <ButtonHome text="Start" onClick={() => alert("Start")} className="startButton" />
+      <div>
+        <div className={styles.homeContainer}>
+          <h1>Bem vindo ao empresário Mario Spalatti</h1>
+          <h1>Welcome to businessman Mario Spalatti</h1>
+          <ButtonHome
+            text="Sobre mim"
+            onClick={() => alert("Sobre mim")}
+            className="sobreMim"
+          />
+          <ButtonHome
+            text="Start"
+            onClick={() => alert("Start")}
+            className="startButton"
+          />
+        </div>
+        <img src={marioImage} alt="Mario" className="marioImage" />
       </div>
-      {/* <img src={marioImage} alt="Mario" className="marioImage"/> */}
     </>
   );
 }
